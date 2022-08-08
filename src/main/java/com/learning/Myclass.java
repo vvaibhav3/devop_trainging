92dcd075752a60ae2eb3ba5f5ebaf6b6514d902f
amdjsfk66 <amdjsfk66@Amdjfs121.iiht.tech>
2022-08-08 09:28:50 +0530

    second commit

--- src/main/java/com/learning/Myclass.java
+++ src/main/java/com/learning/Myclass.java
@@ -5,6 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello world");
+		System.out.print("Hello world2");
 	}
 
 }
